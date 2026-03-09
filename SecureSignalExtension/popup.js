77544be8023afe202caf359a655b4dc87ab82e9b
massarellim <matteo.massarelli@gmail.com>
2026-03-09 17:05:47 +0100

    version 2.0

--- SecureSignalExtension/popup.js
+++ SecureSignalExtension/popup.js
@@ -99,7 +99,8 @@ document.addEventListener('DOMContentLoaded', () => {
                     <path d="M12 9V12M12 16.01L12.01 15.9989M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
                 ${message}
-                ${details ? \`<div class="error-message">\${details}</div>\` : ''}
+                ${details ? `<div class="error-message">${details}</div>` : ''}
             </div>
         `;
-    });
+    }
+});
